80c7a5e1c5f932688c8a63cc659b67f7704dff0b
Guilherme <switchaep@gmail.com>
2023-09-28 18:57:19 -0300

    installing beautiful dnd

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -72,6 +72,7 @@ function App() {
    
   return (
     <div className="App">
+      <h2>Questão</h2>
       {alternativas.map((alternativa) => (
         <Alternative 
           key={alternativa.id}
